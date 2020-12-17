5afa40bc629d96bc251d9a0a5c185cbe0002de62
Wallace Rossini <rossiniwallace@gmail.com>
2020-12-17 09:57:09 -0300

    Config Port

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ app.use(express.json());
 
 app.use(routes);
 
-app.listen(3333, () =>[
-  console.log('Port: 3333')
+var port  = process.env.PORT || 3333;
+
+app.listen(port, () =>[
+  console.log(`Port: ${port}`)
 ])
